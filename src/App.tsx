c2dc109cba7d3e8dd92c0dc5eb70843979213c78
green-mint <saad.521509@gmail.com>
2023-10-01 02:35:42 +0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <title>Bitsmiths - Your Software Solutions</title>
+      <meta
+        name="description"
+        content="We provide software solutions in the domain of web development and AI"
+      />
+
+      <main className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-white font-bold text-7xl">Bitsmiths</h1>
+        <h3 className="text-white text-3xl font-bold">Coming Soon!</h3>
+      </main>
+    </>
+  );
+}
+
+export default App;
